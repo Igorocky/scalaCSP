a753fa7a0c277d7263c096fdf8e0f65b09fd5598
Igorocky <Igorocky@users.noreply.github.com>
2019-01-06 15:01:05 +0100

    Milestone 6.

--- src/main/scala/observatory/Interaction2.scala
+++ src/main/scala/observatory/Interaction2.scala
@@ -9,7 +9,10 @@ object Interaction2 {
     * @return The available layers of the application
     */
   def availableLayers: Seq[Layer] = {
-    ???
+    List(
+      Layer(LayerName.Temperatures, List((0, Color(128, 128, 128))), 1975 to 2015),
+      Layer(LayerName.Deviations, List((2, Color(128, 128, 128))), 1990 to 2015)
+    )
   }
 
   /**
@@ -17,7 +20,7 @@ object Interaction2 {
     * @return A signal containing the year bounds corresponding to the selected layer
     */
   def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = {
-    ???
+    Signal(selectedLayer().bounds)
   }
 
   /**
@@ -29,7 +32,14 @@ object Interaction2 {
     *         in the `selectedLayer` bounds.
     */
   def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year] = {
-    ???
+    Signal(
+      if(selectedLayer().bounds.contains(sliderValue())) {
+        sliderValue()
+      } else {
+        if (sliderValue() < selectedLayer().bounds.start) selectedLayer().bounds.start
+        else selectedLayer().bounds.end
+      }
+    )
   }
 
   /**
@@ -38,7 +48,7 @@ object Interaction2 {
     * @return The URL pattern to retrieve tiles
     */
   def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+    Signal("target/" + selectedLayer().layerName.id + "/" + selectedYear() + "/{z}/{x}/{y}.png'")
   }
 
   /**
@@ -47,7 +57,7 @@ object Interaction2 {
     * @return The caption to show
     */
   def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+    Signal(selectedLayer().layerName.id.capitalize + " (" + selectedYear() + ")")
   }
 
 }
